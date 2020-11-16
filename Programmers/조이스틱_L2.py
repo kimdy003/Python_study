7d5b1eff1606b3fcd4c4aa68a6e846d6098f4374
kimdy003 <kyl123451@naver.com>
2020-11-17 00:08:51 +0900

    Create 조이스틱_L2.py

---
+++ Programmers/조이스틱_L2.py
@@ -0,0 +1,21 @@
+#조이스틱
+
+def solution(name):
+    name_list = [min(ord(i) - ord('A'), ord('Z') - ord(i)+1) for i in name]
+    idx, ans= 0, 0
+    while True:
+        ans += name_list[idx]
+        name_list[idx] = 0
+        if sum(name_list) == 0:
+            break
+            
+        left, right = 1, 1
+        while name_list[idx - left] ==0:
+            left +=1
+        while name_list[idx + right] ==0:
+            right +=1
+        ans += left if left < right else right
+        idx += -left if left < right else right
+    return ans
+
+solution("JESON")
