87d385a3602996128c0e6568f2ac122c60c5327b
kimdy003 <kyl123451@naver.com>
2020-11-22 20:16:01 +0900

    Create 괄호변환_L2.py

---
+++ Programmers/괄호변환_L2.py
@@ -0,0 +1,48 @@
+#괄호변환
+
+def divide(p):
+    count = [0, 0]
+    for i in p:
+        if i == '(':
+            count[0] += 1
+        else:
+            count[1] += 1
+        if count[0] == count[1]:
+            break
+    return p[:sum(count)], p[sum(count):]
+
+
+def check(p):
+    stack = []
+    try:
+        for i in p:
+            if i == '(':
+                stack.append('(')
+            else:
+                stack.pop()
+        return True
+    except:
+        return False
+    
+
+def convert(u):
+    temp = ''
+    for i in u:
+        if i == '(':
+            temp += ')'
+        else:
+            temp += '('
+    return temp
+    
+    
+def solution(p):
+    answer = ''
+    
+    while len(p) != 0:
+        u, p = divide(p)
+        if check(u):
+            answer += u
+        else:
+            answer += '(' + solution(p) + ')' + convert(u[1:-1])
+            break 
+    return answer
