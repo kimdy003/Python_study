5b154f7c7c7cef0aceee817cecc4931271ab0978
kimdy003 <kyl123451@naver.com>
2020-11-30 23:13:30 +0900

    Create 2016년.py

---
+++ Programmers/level_1/2016년.py
@@ -0,0 +1,7 @@
+#2016년
+
+def solution(a, b):
+    month = [31,29,31,30,31,30,31,31,30,31,30,31]
+    day = ["THU","FRI","SAT","SUN","MON","TUE","WED"]
+    
+    return day[(sum(month[:a-1])+b) % 7]
