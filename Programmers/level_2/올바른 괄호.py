a8a0f23c3894c6f2c4b66e7ac97f560d6308cbaf
kimdy003 <kyl123451@naver.com>
2020-12-08 19:06:27 +0900

    Create 올바른 괄호.py

---
+++ Programmers/level_2/올바른 괄호.py
@@ -0,0 +1,13 @@
+def solution(s):
+    stack = []
+    
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            try:
+                stack.pop()
+            except IndexError:
+                return False
+
+    return len(stack) == 0
