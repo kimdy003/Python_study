ac5627aa002c1cd45be55ca1255f77cfc581a6f9
kimdy003 <kyl123451@naver.com>
2020-12-11 21:01:08 +0900

    Create 땅따먹기.py

---
+++ Programmers/level_2/땅따먹기.py
@@ -0,0 +1,10 @@
+def solution(land):
+    
+    N = len(land)-1
+    for i in range(0, N):
+        land[i+1][0] += max(land[i][1], land[i][2], land[i][3])
+        land[i+1][1] += max(land[i][0], land[i][2], land[i][3])
+        land[i+1][2] += max(land[i][0], land[i][1], land[i][3])
+        land[i+1][3] += max(land[i][0], land[i][1], land[i][2])
+        
+    return max(land[N])
