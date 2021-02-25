c575ff4e0ec08176790f3fa0834fcc60b21593c9
kimdy003 <kyl123451@naver.com>
2021-02-25 16:46:42 +0900

    Update 29_스타 수업.py

--- Programmers/level_3/29_스타 수업.py
+++ Programmers/level_3/29_스타 수업.py
@@ -3,9 +3,23 @@ from collections import Counter
 def solution(array):
     answer = -1
     element = Counter(array)
-    print(element)
 
-        
-    return answer
+    for star in element.keys():
+        if element[star] <= answer:
+            continue
+
+        cnt = 0
+        idx = 0
+        while idx < len(array)-1:
+            if(array[idx] == array[idx+1]) or(array[idx] != star and array[idx+1] != star):
+                idx += 1
+                continue
+
+            cnt += 1
+            idx += 2
+        answer = max(answer, cnt)
+    
+
+    return answer*2 if answer != -1 else 0
 
 print(solution([5,2,3,3,5,3]))
