6e92014cd45508e686f8832cba538bd8186cb571
kimdy003 <kyl123451@naver.com>
2021-02-25 16:38:17 +0900

    Create 29_스타 수업.py

---
+++ Programmers/level_3/29_스타 수업.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def solution(array):
+    answer = -1
+    element = Counter(array)
+    print(element)
+
+        
+    return answer
+
+print(solution([5,2,3,3,5,3]))
