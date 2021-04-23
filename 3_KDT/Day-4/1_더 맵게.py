88c861d4d4312d41b322f879ad40608a7e2044e9
kimdy003 <kyl123451@naver.com>
2021-04-23 15:54:06 +0900

    KDT_Day_04

---
+++ 3_KDT/Day-4/1_더 맵게.py
@@ -0,0 +1,29 @@
+#
+#  1_더 맵게.py
+#  더 맵게
+#
+#  Create by 김도영 on 2021/04/23
+#
+
+
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while True:
+        min1 = heapq.heappop(scoville)
+        if min1 >= K:
+            break
+        elif len(scoville) == 0:
+            answer = -1
+            break
+
+        min2 = heapq.heappop(scoville)
+        new_scoville = min1 + 2 * min2
+        heapq.heappush(scoville, new_scoville)
+        answer += 1
+
+    return answer
