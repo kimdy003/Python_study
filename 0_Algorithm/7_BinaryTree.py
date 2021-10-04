c614ababe7b951a2df94b22604388825dce90993
DoyoungKim <kdzero0317@gmail.com>
2021-10-05 02:20:19 +0900

    Update 7_BinaryTree.py

--- 0_Algorithm/7_BinaryTree.py
+++ 0_Algorithm/7_BinaryTree.py
@@ -1,20 +1,69 @@
 class Node:
-    def __init__(self, item):
-        self.data = item
+    def __init__(self, data: int):
+        self.data = data
         self.left = None
         self.right = None
 
-    def size(self):
-        l = self.left.size() if self.left else 0
-        r = self.right.size() if self.right else 0
-        return l + r + 1
 
 class BinaryTree:
-    def __init__(self, r):
-        self.root = r
+    def __init__(self):
+        self.root = None
 
-    def size(self):
-        if self.root:
-            return self.root.size()
+    def insert(self, root: Node, data: int):
+        if root is None:
+            root = Node(data)
+        elif data < root.data:
+            root.left = self.insert(root.left, data)
+        elif data > root.data:
+            root.right = self.insert(root.right, data)
+        return root
+
+    # Search
+    def Search(self, root: Node, key: int):
+        if root is None or root.data is key:
+            return root
+        elif key < root.data:
+            return self.Search(root.left, key)
+        elif key > root.data:
+            return self.Search(root.right, key)
+
+    # 전이 순회
+    def PreOrder(self, root: Node):
+        if root is None:
+            pass
         else:
-            return 0
+            print(root.data)
+            self.PreOrder(root.left)
+            self.PreOrder(root.right)
+
+    # 중위 순회
+    def InOrder(self, root: Node):
+        if root is None:
+            pass
+        else:
+            self.InOrder(root.left)
+            print(root.data)
+            self.InOrder(root.right)
+
+    # 후위 순회
+    def PostOrder(self, root: Node):
+        if root is None:
+            pass
+        else:
+            self.PostOrder(root.left)
+            self.PostOrder(root.right)
+            print(root.data)
+
+    # 레벨 순회
+    def LevelOrder(self, root: Node):
+        from collections import deque
+
+        queue = deque()
+        queue.append(root)
+        while queue:
+            node = queue.popleft()
+            print(node.data)
+            if node.left is not None:
+                queue.append(node.left)
+            if node.right is not None:
+                queue.append(node.right)
